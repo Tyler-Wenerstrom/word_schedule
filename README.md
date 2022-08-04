dec0eda2f9b980a29cba4eaff23ccfceea296b54
Tyler <twenerstrom@gmail.com>
2022-08-04 15:00:02 -0600

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # word_schedule
+This is just a sample page of my very amateur coding skills. Hope to improve soon!
+
+## Challenges
+In this challenge there was a main focus on organization and flex styling along with a few links and responsive pictures. I aslo attempted to optimize the page for various viewports.
+
+## Links
+https://tyler-wenerstrom.github.io/TRW-Portfolio/
+
+<img src= "images\work day schedule screenshot.png" width="auto" height="475" alt="Screenshot"/>
+
+## Author
+https://github.com/Tyler-Wenerstrom
--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-$(init);
-function init(){
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    setinterval(60000)
+var description = $(".description");
+var value = $(this).siblings(".description").val();
+var timeEl = $(this).parent().attr("id");
+var currentHour = moment().format("H");
+function getDate() {
+  var today = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(today);
 }
+getTask();
+getDate();
+// getTask();
 
+$(".time-block").each(function () {
+  var getHourId = parseInt($(this).attr("id").split("-")[1]);
+  $(this).removeClass("past");
+  if (currentHour < getHourId) {
+    $(this).addClass("future");
+  } else if (currentHour == getHourId) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("past");
+  }
+});
 
-
-
-
-
-// $(document).ready(function(event){
-    $("#submit").click(function(){
-        var inputData = $("#comment").textContent;
-       localStorage.setItem("schedule1", JSON.stringify(inputData))
-    console.log(inputData); 
-    });
-
-// var testData = document.querySelector("#test").value;
-// var testButton = document.querySelector("#press")
-
-
-// testButton.addEventListener('click', function(){
-//     localStorage.setItem("test", testData)
-// })
-
-
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".description").val();
+  var timeEl = $(this).parent().attr("id");
+  localStorage.setItem(timeEl, JSON.stringify(value));
+});
+function getTask() {
+  $("#07").text(localStorage.getItem("hour-7"));
+  $("#08").val(localStorage.getItem("hour-8"));
+  $("#09").val(localStorage.getItem("hour-9"));
+  $("#10").val(localStorage.getItem("hour-10"));
+  $("#11").val(localStorage.getItem("hour-11"));
+  $("#12").val(localStorage.getItem("hour-12"));
+  $("#13").val(localStorage.getItem("hour-13"));
+  $("#14").val(localStorage.getItem("hour-14"));
+  $("#15").val(localStorage.getItem("hour-15"));
+  $("#16").val(localStorage.getItem("hour-16"));
+  $("#17").val(localStorage.getItem("hour-17"));
+}
+// console.log(getTask)
+// // }
+// let storedItems = localStorage.getItem('time')
+// $('.description').innerHTML= storedItems;
+//  };
